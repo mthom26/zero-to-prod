c4a962e287195def691d743788588f771815198b
mthom26 <mthom2626@gmail.com>
2022-04-18 16:29:04 +0100

    Add subscribe route and tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -18,6 +18,54 @@ async fn health_check_works() {
     assert_eq!(response.content_length(), Some(0));
 }
 
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    let server_addr = serve();
+    let addr = format!("http://{}/subscriptions", server_addr);
+
+    let body = "name=jimmy%20derp&email=jimderp%40derpmail.com";
+    let client = reqwest::Client::new();
+    let response = client
+        .post(addr)
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Could not execute request.");
+
+    assert_eq!(response.status(), 200);
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_for_invalid_form_data() {
+    let server_addr = serve();
+    let addr = format!("http://{}/subscriptions", server_addr);
+
+    let test_bodies = [
+        ("name=jimmy%20derp", "missing email"),
+        ("email=jimderp%40derpmail.com", "missing name"),
+        ("", "missing name and email"),
+    ];
+
+    for (body, error) in test_bodies {
+        let client = reqwest::Client::new();
+        let response = client
+            .post(addr.clone())
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Could not execute request.");
+
+        assert_eq!(
+            response.status(),
+            400,
+            "The API did not fail when {}",
+            error
+        )
+    }
+}
+
 fn serve() -> SocketAddr {
     let addr = SocketAddr::from(([127, 0, 0, 1], 0));
     let app = app();
